6d1da3d88952439fa279f7e33a3d300fc81e3677
Robin RT. TUSSEAU <Robin.TUSSEAU@etskirsch.fr>
2017-11-27 15:37:47 +0100

    Correction for unit tests

--- back/server.js
+++ back/server.js
@@ -6,7 +6,8 @@ bodyParser = require('body-parser');
 console.log('todo list RESTful API server started on: ' + port);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'}));  
 var routes = require('./api/routes/validationRoutes'); //importing route
 routes(app); //register the route
 
--- back/tests/validationTests.js
+++ back/tests/validationTests.js
@@ -4,70 +4,75 @@ let chaiHttp = require('chai-http');
 var validationController = require('../api/controllers/validationController');
 var http = require('http');
 let server = require("../server");
+const should = chai.should();
 chai.use(chaiHttp);
-describe('validationController', () => {
+describe('Validation', () => {
     describe('POST /validate/mail', () => {
 
-    it('validateEmail should return false if the mail passed is not valid', () => {
+    it('validateEmail should return false if the mail passed is not valid', (done) => {
         chai.request(server)
         .post('/validate/email')
         .send({mail:"test@test"})
         .end((err,res)=>{
             res.should.have.status(200);
-            res.body.should.be.equal.to(false);
+            res.body.should.be.eql(false);
             done();
         })
     });
-    it('validateEmail should return false if the mail passed is empty', () => {
+    it('validateEmail should return false if the mail passed is empty', (done) => {
         chai.request(server)
         .post('/validate/email')
         .send({mail:null})
         .end((err,res)=>{
             res.should.have.status(200);
-            res.body.should.be.equal.to(false);
+            res.body.should.be.eql(false);
             done();
         })
     });
-    it('validateEmail should return true if the mail passed is valid', () => {
+    it('validateEmail should return true if the mail passed is valid', (done) => {
         chai.request(server)
         .post('/validate/email')
         .send({mail:"test@test.com"})
         .end((err,res)=>{
             res.should.have.status(200);
-            res.body.should.be.equal.to(true);
+            res.body.should.be.eql(true);
             done();
         })
     })
 });
     describe( 'POST /validate/password', () => {
 
-    it('validatePassword should return false if the password is not long enough', () => {
+    it('validatePassword should return false if the password is not long enough', (done) => {
+        let password = {password:"tesT"}; 
         chai.request(server)
         .post('/validate/password')
-        .send({password:"tesT"})
+        .send(password)
         .end((err,res)=>{
             res.should.have.status(200);
-            res.body.should.be.equal.to(false);
+            res.body.should.be.eql(false);
             done();
         })
     })
-    it('validatePassword should return false if the password does not contain an upperCasse', () => {
+    it('validatePassword should return false if the password does not contain an upperCasse', (done) => {
         chai.request(server)
         .post('/validate/password')
         .send({password:"testt"})
         .end((err,res)=>{
+            should.exist(res);
             res.should.have.status(200);
-            res.body.should.be.equal.to(false);
+            should.exist(res.body);
+            res.body.should.be.eql(false);
             done();
         })
     })
-    it('validatePassword should return true if the password contains an upperCasse and has at least 5 char', () => {
+    it('validatePassword should return true if the password contains an upperCasse and has at least 5 char', (done) => {
         chai.request(server)
         .post('/validate/password')
         .send({password:"Testt"})
         .end((err,res)=>{
+            console.log(res.body);
             res.should.have.status(200);
-            res.body.should.be.equal.to(true);
+            res.body.should.be.eql(true);
             done();
         })
     })
